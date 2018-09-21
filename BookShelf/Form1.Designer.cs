8af22d1d674872f0ef770003fcc53a0e5e1909e3
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-21 10:38:31 -0500

    binded controls to data table

--- BookShelf/Form1.Designer.cs
+++ BookShelf/Form1.Designer.cs
@@ -29,21 +29,103 @@
         private void InitializeComponent()
         {
             this.txtConn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.txtPubID = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtConn
             // 
-            this.txtConn.Location = new System.Drawing.Point(94, 408);
+            this.txtConn.Location = new System.Drawing.Point(70, 332);
+            this.txtConn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtConn.Name = "txtConn";
-            this.txtConn.Size = new System.Drawing.Size(614, 22);
+            this.txtConn.Size = new System.Drawing.Size(462, 20);
             this.txtConn.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Year published";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Publisher ID";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(273, 32);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 5;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(273, 86);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 20);
+            this.txtYear.TabIndex = 6;
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(273, 142);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(100, 20);
+            this.txtISBN.TabIndex = 7;
+            // 
+            // txtPubID
+            // 
+            this.txtPubID.Location = new System.Drawing.Point(273, 192);
+            this.txtPubID.Name = "txtPubID";
+            this.txtPubID.Size = new System.Drawing.Size(100, 20);
+            this.txtPubID.TabIndex = 8;
             // 
             // frmTitles
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.txtPubID);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtConn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmTitles";
             this.Text = "Titles Database";
             this.Load += new System.EventHandler(this.frmTitles_Load);
@@ -55,6 +137,14 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtConn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.TextBox txtPubID;
     }
 }
 
--- BookShelf/Form1.cs
+++ BookShelf/Form1.cs
@@ -15,7 +15,8 @@ namespace BookShelf
     {
         OleDbConnection conn;
         OleDbCommand titlesCommand;
-        OleDbDataAdapter titlesAdapter;  
+        OleDbDataAdapter titlesAdapter;
+        DataTable titlesTable;
 
         public frmTitles()
         {
@@ -24,18 +25,37 @@ namespace BookShelf
 
         private void frmTitles_Load(object sender, EventArgs e)
         {
-            var connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\ysong\source\repos\BookShelf\Books.accdb;
+            var connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\jchen\source\repos\BookShelf\Books.accdb;
                                 Persist Security Info = False;";
 
+            // create conection
             conn = new OleDbConnection(connString);
             conn.Open();
+
+            // create sql command
             titlesCommand = new OleDbCommand("Select * from Titles",conn);
+
+            // run the sql code
             titlesAdapter = new OleDbDataAdapter();
             titlesAdapter.SelectCommand = titlesCommand;
 
+            // create data table and fill the table with the query result
+            titlesTable = new DataTable();
+            titlesAdapter.Fill(titlesTable);
+
+            // bind controls with data
+            txtTitle.DataBindings.Add("Text", titlesTable, "Title");
+            txtYear.DataBindings.Add("Text", titlesTable, "Year_Published");
+            txtISBN.DataBindings.Add("Text", titlesTable, "ISBN");
+            txtPubID.DataBindings.Add("Text", titlesTable, "PubID");
+
+            // close connection
             conn.Close();
             conn.Dispose();
             titlesAdapter.Dispose();
+            titlesTable.Dispose();
+
+           
         }
     }
 }
